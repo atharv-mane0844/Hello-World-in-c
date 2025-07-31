8f3d8d0e20f81c5e808da0423356b6c1f85b41fc
Atharv Mane <atharvmane0844@gmail.com>
2025-07-31 23:28:07 +0530

    Add files via upload

---
+++ additon.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a,b,add;
+    cout<<"enter two numbers \n";
+    cin>>a>>b;
+
+    add=a+b;
+
+    cout<<"addition is :"<<add<<endl;
+
+    return 0;
+}
